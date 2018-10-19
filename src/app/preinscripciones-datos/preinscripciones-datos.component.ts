ec13027ba429f8e6057af149a6537c214e7f2cac
Evelyn Guadalupe Castro Galaviz <evelyn_castro96@hotmail.com>
2018-10-18 23:55:31 -0600

    evelyn datos3

--- src/app/preinscripciones-datos/preinscripciones-datos.component.ts
+++ src/app/preinscripciones-datos/preinscripciones-datos.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preinscripciones-datos.component.css']
 })
 export class PreinscripcionesDatosComponent implements OnInit {
-
+  //configuración para select
+  config = {
+    multiple:false,
+    //value:la variable de modelo en la que desea guardar las opciones seleccionadas.
+    displayKey:"description", 
+    search:true 
+  }
+  dataModel:any[] = ['101','102','103']
   constructor() { }
 
   ngOnInit() {
