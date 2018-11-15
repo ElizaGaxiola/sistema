3a31ae541acdd2f2026df620be5607f0e252ecb6
Evelyn Guadalupe Castro Galaviz <evelyn_castro96@hotmail.com>
2018-11-14 22:46:38 -0700

    MODIFICAR-ACTUALIZACIONES

--- src/app/modificar-perfil-alumno/modificar-perfil-alumno.component.ts
+++ src/app/modificar-perfil-alumno/modificar-perfil-alumno.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificar-perfil-alumno.component.css']
 })
 export class ModificarPerfilAlumnoComponent implements OnInit {
-
+ //configuración para select
+ config = {
+  multiple:false,
+  //value:la variable de modelo en la que desea guardar las opciones seleccionadas.
+  displayKey:"description", 
+  search:true 
+}
+dataModel:any[] = ['101','102','103']
   constructor() { }
 
   ngOnInit() {
