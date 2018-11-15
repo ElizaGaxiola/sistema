6dc74da89f8f7f2119b8a5caf07d7e4af332383a
ElizaGaxiola <42920473+ElizaGaxiola@users.noreply.github.com>
2018-11-15 00:23:50 -0700

    nuevo

--- src/app/caja/caja.component.ts
+++ src/app/caja/caja.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CajaComponent implements OnInit {
   modulo: string ='Caja';
+  dtOptions: DataTables.Settings = {};
+  //datos para datatable
+  data: any[]=[
+    { matricula: "12344", apellidop:"Castro", apellidom:"Galaviz", nombre:"Evelyn Guadalupe",  calificacion:"10", faltas: "0"},
+    { matricula: "12345", apellidop:"Chávez", apellidom:"Lizárraga", nombre:"Jesús Antonio",  calificacion:"10", faltas: "0"},
+    { matricula: "12346", apellidop:"Gaxiola", apellidom:"Carrillo", nombre:"Elizabeth",  calificacion:"10", faltas: "0"},
+    { matricula: "12347", apellidop:"Paredes", apellidom:"Lopez", nombre:"Julio Enrique",  calificacion:"10", faltas: "0"},
+    { matricula: "12348", apellidop:"Valenzuela", apellidom:"Camacho", nombre:"Grace de Jesús",  calificacion:"10", faltas: "0"},
+    { matricula: "12349", apellidop:"Vazquez", apellidom:"Niebla", nombre:"Kevin David", calificacion:"10"},
+  ];
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.dtOptions = {
+      language: {
+        "emptyTable": "Sin resultados encontrados",
+        "info": " _START_ - _END_ / _TOTAL_ ",
+        "infoEmpty": "0-0 /0",
+        "infoFiltered": "",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ registros",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+      }
+    };
   }
 
 }
