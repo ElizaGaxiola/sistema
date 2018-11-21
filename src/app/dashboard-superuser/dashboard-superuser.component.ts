afa3807f7f817230fc2295e593201e0d8cd4216f
Evelyn Guadalupe Castro Galaviz <evelyn_castro96@hotmail.com>
2018-11-21 12:31:35 -0700

    superusuario

--- src/app/dashboard-superuser/dashboard-superuser.component.ts
+++ src/app/dashboard-superuser/dashboard-superuser.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-superuser.component.css']
 })
 export class DashboardSuperuserComponent implements OnInit {
-
+  modulo:string='Dashboard';
   constructor() { }
 
   ngOnInit() {
