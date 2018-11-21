766d19b80f84a8b2959826d152640a1283703352
ElizaGaxiola <42920473+ElizaGaxiola@users.noreply.github.com>
2018-11-21 12:10:07 -0700

    auth

--- src/app/index-admin/index-admin.component.ts
+++ src/app/index-admin/index-admin.component.ts
@@ -17,35 +17,51 @@ export class IndexAdminComponent implements OnInit {
   public usuarioData:any = { idUsuario:'', idTipo: '', usuario: '', password:'' };
 
   constructor(private router: Router, private auth: AuthService) { }
-  public submit() {
+  private autenticarDocente() {
     this.auth.login(this.usuario, this.password )
       .pipe(first())
-      .subscribe((data: {}) => {
-        this.getUser();
-        if (this.usuarioData.idTipo == 1){
-          console.log('Docente');
-          this.router.navigate(['docente/dashboard']);
-        }else if (this.usuarioData.idTipo == 3){
-          console.log('Admistrador');
-          this.router.navigate(['administrativos/dashboard']);
-        }else if (this.usuarioData.idTipo == 4){
-          console.log('SuperUsuario');
-          this.router.navigate(['superusuario/dashboard']);
-        }else{
-          this.error = 'Datos erroneos'
-        }
-      });
+      .subscribe(
+        result => this.router.navigate(['docente/dashboard']),
+        err => this.error = 'Datos erroneos'
+      );
+  }
+  private autenticarAdministrador() {
+    this.auth.login(this.usuario, this.password )
+      .pipe(first())
+      .subscribe(
+        result => this.router.navigate(['administrativos/dashboard']),
+        err => this.error = 'Datos erroneos'
+      );
+  }
+  private autenticarSuperUsuario() {
+    this.auth.login(this.usuario, this.password )
+      .pipe(first())
+      .subscribe(
+        result => this.router.navigate(['superusuario/dashboard']),
+        err => this.error = 'Datos erroneos'
+      );
   }
 
-  private getUser(){
+
+  private submit(){
     this.auth.getUser(this.usuario)
     .subscribe((data: any) => {
       this.usuarioData.idUsuario=data.idUsuario;
       this.usuarioData.idTipo=data.idTipo;
       this.usuarioData.usuario=data.usuario;
       this.usuarioData.password=data.password;
+      if ( this.usuarioData.idTipo == 1){
+        this.autenticarDocente();
+      }else if(this.usuarioData.idTipo == 3){
+        this.autenticarAdministrador();
+      }else if(this.usuarioData.idTipo == 4){
+        this.autenticarSuperUsuario();
+      }else{
+        this.error = 'Datos erroneos';
+      }
     });
   }
+  
   ngOnInit() {
   }
 
