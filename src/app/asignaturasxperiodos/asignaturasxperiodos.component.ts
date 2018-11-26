ae7d852c7a89f917afd4f9a89fd3f5f7ace57c41
Evelyn Guadalupe Castro Galaviz <evelyn_castro96@hotmail.com>
2018-11-25 19:27:14 -0700

    componentasignaturasxperiodo

--- src/app/asignaturasxperiodos/asignaturasxperiodos.component.ts
+++ src/app/asignaturasxperiodos/asignaturasxperiodos.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
+import { Http, Response } from '@angular/http';
+import { AsignaturaPeriodo } from '../modelos';
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-asignaturasxperiodos',
@@ -6,10 +11,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asignaturasxperiodos.component.css']
 })
 export class AsignaturasxperiodosComponent implements OnInit {
+  modulo:string='Asignaturas Por Periodo';
+  modal:string;
+  periodoSelect:any[]=[];
+  idPeriodo: any;
+  asignaturaSelect:any[]=[];
+  idMateria: any;
+  carreraSelect:any[]=[];
+  idCarrera:any;
+  seccionSelect:any[]=[];
+  idSeccion:any;
+  dataModel:any[] = ['101','102','103']
+  asignaturaPeriodoForm: FormGroup;
+  asignaturaPeriodo: AsignaturaPeriodo;
+  
+  dtOptions: DataTables.Settings = {};
+  data: any[]=[
+    { nombre: "1", creditos:"Acosta"},
+    
+  ];
+  constructor(private pf: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  public agregar(){
+    this.inicializarForm();
+    this.modal = 'agregar';
+    $("#modal-modificar").modal();
+  }
+  
+  public inicializarForm(){
+    this.asignaturaPeriodoForm = this.pf.group({
+      idMateria:['',[Validators.required]],
+      idCarrera: ['',[Validators.required]],
+      idSeccion: ['',[Validators.required]],
+      year: ['',[Validators.required]],
+      idPeriodo: ['',[Validators.required]],
+      creditos:['1'],
+    });
+   }
+  ngOnInit(): void {
+    this.dtOptions = {
+      language: {
+        "emptyTable": "Sin resultados encontrados",
+        "info": " _START_ - _END_ / _TOTAL_ ",
+        "infoEmpty": "0-0 /0",
+        "infoFiltered": "",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ registros",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "<i class='fas fa-search'></i>",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+      }
+    };
+  }
+ 
+  onSubmit(){
+    this.asignaturaPeriodo = this.saveAsignaturaPeriodo();
+  }
+  saveAsignaturaPeriodo(){
+    const saveAsignaturaPeriodo={
+      idMateria: this.asignaturaPeriodoForm.get('idMateria').value,
+      idCarrera: this.asignaturaPeriodoForm.get('idCarrera').value,
+      idSeccion: this.asignaturaPeriodoForm.get('idSeccion').value,
+      year: this.asignaturaPeriodoForm.get('year').value,
+      idPeriodo: this.asignaturaPeriodoForm.get('idPeriodo').value,
+      creditos: this.asignaturaPeriodoForm.get('creditos').value,
+    };
+    return saveAsignaturaPeriodo;
   }
 
 }
