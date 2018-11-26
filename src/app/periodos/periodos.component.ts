17c7a9a8fd0fb455f26ba078a9adfc670c84f00f
Evelyn Guadalupe Castro Galaviz <evelyn_castro96@hotmail.com>
2018-11-25 17:43:15 -0700

    periodos

--- src/app/periodos/periodos.component.ts
+++ src/app/periodos/periodos.component.ts
@@ -1,5 +1,8 @@
 import { Component,OnDestroy, OnInit } from '@angular/core';
+import { AbcService } from '../abc.service';
+import { FormControl, FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 import { Http, Response } from '@angular/http';
+import { Periodo } from '../modelos';
 declare var jQuery:any;
 declare var $:any;
 @Component({
@@ -8,31 +11,50 @@ declare var $:any;
   styleUrls: ['./periodos.component.css']
 })
 export class PeriodosComponent implements OnInit {
-  modulo:string='Periodos por Sección';
-      //configuración para select
- config = {
-  multiple:false,
-  //value:la variable de modelo en la que desea guardar las opciones seleccionadas.
-  displayKey:"description", 
-  search:true 
-}
+modulo:string='Periodos por Sección';
+periodoForm:FormGroup;
+periodo: Periodo;
+modal:string;
+idSeccion:any;
+seccionesSelect: any[]=[];
+idCarrera:any;
+carreraSelect: any[]=[];
 dataModel:any[] = ['101','102','103']
   dtOptions: DataTables.Settings = {};
   data: any[]=[
     { descripcion:"Acosta", seccion:"07/08/2018", carrera:"07/07/2019"},
     
   ];
-  constructor() { }
+  constructor( private pf: FormBuilder) { }
 
   public agregar(){
- 
-    $("#modal-agregar").modal();
-  }
-  public modificar(){
-    
+    this.inicializarForm();
+    this.modal = 'agregar';
     $("#modal-modificar").modal();
   }
+  public onSubmit(){
+    this.periodo = this.savePeriodo();
+  }
 
+  public savePeriodo(){
+    const savePeriodo ={
+        idSeccion: this.periodoForm.get('idSeccion').value,
+        idCarrera: this.periodoForm.get('idCarrera').value,
+        idPeriodo: this.periodoForm.get('idPeriodo').value,
+        descripcion: this.periodoForm.get('descripcion').value,
+    }
+    return savePeriodo;
+  }
+
+  public inicializarForm(){
+    this.periodoForm = this.pf.group({
+      idSeccion: ['',[ Validators.required]],
+      idCarrera: ['',[ Validators.required]],
+      descripcion: ['',[ Validators.required]],
+      idPeriodo: [''],
+    });
+   }
+   
   ngOnInit(): void {
     this.dtOptions = {
       language: {
